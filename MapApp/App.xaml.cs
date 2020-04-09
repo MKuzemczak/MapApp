8a468389e3ace0d70ad73bb445c46f51b601ce8c
Michał Kuzemczak <kuzemczak.michal@gmail.com>
2020-04-09 13:34:09 +0200

    mainpage, split view

--- MapApp/App.xaml.cs
+++ MapApp/App.xaml.cs
@@ -39,7 +39,7 @@ namespace MapApp
 
         private ActivationService CreateActivationService()
         {
-            return new ActivationService(this, typeof(Views.MapPage), new Lazy<UIElement>(CreateShell));
+            return new ActivationService(this, typeof(Views.MainPage), new Lazy<UIElement>(CreateShell));
         }
 
         private UIElement CreateShell()
