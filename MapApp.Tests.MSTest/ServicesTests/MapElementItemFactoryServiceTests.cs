3d29fcb99cfb0098ee5b03a0a3e04e1de898570e
Michał Kuzemczak <kuzemczak.michal@gmail.com>
2020-04-18 12:18:26 +0200

    MapElementFactoryServiceTests

---
+++ MapApp.Tests.MSTest/ServicesTests/MapElementItemFactoryServiceTests.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using MapApp.Models;
+using MapApp.Services;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.VisualStudio.TestTools.UnitTesting.AppContainer;
+using Windows.Devices.Geolocation;
+using Windows.UI;
+
+namespace MapApp.Tests.MSTest.ServicesTests
+{
+    [TestClass]
+    public class MapElementItemFactoryServiceTests
+    {
+        [UITestMethod]
+        public void GetMapIconItem_ConstructsMapIconItemCorrectly()
+        {
+
+            var expectName = "TestName";
+            var expectPosition = new BasicGeoposition() { Latitude = 1, Longitude = 2 };
+            var expectLayer = new MapLayerItem() { Id = 3, Name = "TestLayer" };
+            int expectId = 4;
+
+            var icon = MapElementItemFactoryService.GetMapIconItem(expectName, expectPosition, expectLayer, expectId);
+
+            Assert.AreEqual(expectName, icon.Name);
+            Assert.AreEqual(expectPosition, icon.GetPosition());
+            Assert.AreEqual(expectLayer, icon.ParentLayer);
+            Assert.AreEqual(expectId, icon.Id);
+        }
+
+        [UITestMethod]
+        public void  GetMapPolylineItem_ConstructsMapPolylineItemCorrectly()
+        {
+            var expectName = "TestName";
+            var expectPath = new List<BasicGeoposition>()
+            {
+                new BasicGeoposition() { Latitude = 1, Longitude = 2 },
+                new BasicGeoposition() { Latitude = 2, Longitude = 3 },
+                new BasicGeoposition() { Latitude = 3, Longitude = 4 }
+            };
+            var expectLayer = new MapLayerItem() { Id = 3, Name = "TestLayer" };
+            int expectId = 5;
+            double expectWidth = 0.00006;
+            var expectStrokeColor = Color.FromArgb(100, 100, 100, 100);
+
+            var polyline = MapElementItemFactoryService.GetMapPolylineItem(
+                expectName, expectPath, expectLayer, expectStrokeColor, expectWidth, expectId);
+
+            Assert.AreEqual(expectName, polyline.Name);
+            Assert.AreEqual(expectLayer, polyline.ParentLayer);
+            Assert.AreEqual(expectId, polyline.Id);
+            Assert.AreEqual(expectWidth, polyline.Width, 0.00000001);
+            Assert.AreEqual(expectStrokeColor, polyline.StrokeColor);
+            Assert.IsTrue(expectPath.SequenceEqual(polyline.Path));
+        }
+
+        [UITestMethod]
+        public void GetMapPolygonItem_ConstructMapPolygonItemCorrectly()
+        {
+            var expectName = "TestName";
+            var expectPath = new List<BasicGeoposition>()
+            {
+                new BasicGeoposition() { Latitude = 1, Longitude = 2 },
+                new BasicGeoposition() { Latitude = 2, Longitude = 3 },
+                new BasicGeoposition() { Latitude = 3, Longitude = 4 }
+            };
+            var expectLayer = new MapLayerItem() { Id = 3, Name = "TestLayer" };
+            int expectId = 5;
+            var expectStrokeColor = Color.FromArgb(100, 100, 100, 100);
+            var expectFillColor = Color.FromArgb(2, 2, 2, 2);
+
+            var polygon = MapElementItemFactoryService.GetMapPolygonItem(
+                expectName, expectPath, expectLayer, expectStrokeColor, expectFillColor, expectId);
+
+            Assert.AreEqual(expectName, polygon.Name);
+            Assert.AreEqual(expectLayer, polygon.ParentLayer);
+            Assert.AreEqual(expectId, polygon.Id);
+            Assert.AreEqual(expectStrokeColor, polygon.StrokeColor);
+            Assert.AreEqual(expectFillColor, polygon.FillColor);
+            Assert.IsTrue(expectPath.SequenceEqual(polygon.Path));
+        }
+    }
+}
--- MapApp/Models/MapLayerItem.cs
+++ MapApp/Models/MapLayerItem.cs
@@ -20,5 +20,67 @@ namespace MapApp.Models
         /// Name of the layer.
         /// </summary>
         public string Name { get; set; }
+
+        /// <summary>
+        /// Compares this object to the object given as argument.
+        /// </summary>
+        /// <param name="obj">Object to compare to</param>
+        /// <returns>True if objects are equal.</returns>
+        public override bool Equals(object obj)
+        {
+            return obj is MapLayerItem item &&
+                   Id == item.Id &&
+                   Name == item.Name;
+        }
+
+        /// <summary>
+        /// Gets hash code of this object.
+        /// </summary>
+        /// <returns>Hash code.</returns>
+        public override int GetHashCode()
+        {
+            int hashCode = -1919740922;
+            hashCode = hashCode * -1521134295 + Id.GetHashCode();
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(Name);
+            return hashCode;
+        }
+
+        /// <summary>
+        /// Equality operator.
+        /// </summary>
+        /// <param name="l1"></param>
+        /// <param name="l2"></param>
+        /// <returns>True if objects are equal.</returns>
+        public static bool operator ==(MapLayerItem l1, MapLayerItem l2)
+        {
+            if ((l1 is object && l2 is null) ||
+                (l1 is null && l2 is object))
+                return false;
+
+            if (l1 is null && l2 is null)
+                return true;
+
+            return (l1.Name == l2.Name && l1.Id == l2.Id);
+        }
+
+        /// <summary>
+        /// Inequality operator.
+        /// </summary>
+        /// <param name="l1"></param>
+        /// <param name="l2"></param>
+        /// <returns>True if object aren't equal.</returns>
+        public static bool operator !=(MapLayerItem l1, MapLayerItem l2)
+        {
+            if ((l1 is object && l2 is null) ||
+                (l1 is null && l2 is object))
+                return true;
+
+            if (l1 is null && l2 is null)
+                return false;
+
+            return !(l1.Name == l2.Name && l1.Id == l2.Id);
+        }
+
+
     }
 }
